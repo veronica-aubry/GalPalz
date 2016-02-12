367a7ed5d77ab5adc5c149685feec2f25741b17b
Veronica <veronica.aubry.alley@gmail.com>
2016-02-12 09:05:41 -0800

    add module content

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,5 +1,7 @@
 
 using Nancy;
+using Contacts.Objects;
+using System.Collections.Generic;
 
 namespace Project
 {
@@ -7,7 +9,38 @@ namespace Project
   {
     public HomeModule()
     {
-      Get["/"] = _ => View ["index.html", };
+      Get["/"] = _ => View ["index.html"];
+      //loads index view at root//
+
+      Get["/add_contact"] = _ => {
+      return View["addContact.cshtml"];
+      };
+
+      //takes user to contact form//
+
+      Post["/contact_created"] = _ => {
+      var newContact = new Contact(Request.Form["contact-name"], Request.Form["contact-number"], Request.Form["contact-address"] );
+      return View["contactCreated.cshtml", newContact];
+    };
+
+    //creates new contact and uses it as model for contact added page//
+
+    Get["/view_contacts"] = _ => {
+    var allContacts = Contact.GetAllContacts();
+    return View["viewContacts.cshtml", allContacts];
+    };
+
+    //gathers all contacts and uses list as model for page that lists all added contacts//
+
+    Get["/contacts_cleared"] = _ => {
+    var allContacts = Contact.GetAllContacts();
+    allContacts.ClearAllContacts();
+    return View["contactsCleared.cshtml"];
+    };
+
+
+
     }
+
   }
 }
