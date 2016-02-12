edbc72412ec0517b56d59d6465d2473c21a34183
Veronica <veronica.aubry.alley@gmail.com>
2016-02-12 08:33:19 -0800

    add initial files

---
+++ Objects/ContactObjects.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+
+namespace Contacts.Objects
+
+{
+
+  //contact variables//
+
+    public class Contact
+  {
+      private string _name;
+      private string _number;
+      private string _address;
+      private int _id;
+      private static List<Contact> _instances = new List<Contact> {};
+
+//Contact construtor//
+
+  public Contact(string contactName, string contactNumber, string contactAddress)
+   {
+     _name = contactName;
+     _number = contactNumber;
+     _address = contactAddress;
+     _instances.Add(this);
+     _id = _instances.Count;
+   }
+
+
+//getters and setters for all Contact properties//
+
+      public string GetName()
+      {
+      return _name;
+      }
+
+      public void SetName(string newName)
+      {
+        _name = newName;
+      }
+
+      public string GetNumber()
+      {
+      return _number;
+      }
+
+      public void SetNumber(string newNumber)
+      {
+        _number = newNumber;
+      }
+
+      public string GetAddress()
+      {
+      return _address;
+      }
+
+      public void SetAddress(string newAddress)
+      {
+        _address = newAddress;
+      }
+
+      public int GetId()
+    {
+      return _id;
+    }
+
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+//methods for all Contact objects//
+
+     public static List<Contact> GetAllContacts()
+     {
+       return _instances;
+     }
+
+     public static void ClearAllContacts()
+    {
+      _instances.Clear();
+    }
+
+  }
+}
